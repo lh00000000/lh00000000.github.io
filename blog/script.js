eee020c444f99f0d53a88401e52d90941a9cca5e
Luming Hao <lh00000000@gmail.com>
2019-04-19 22:54:31 -0400

    freestoremac

--- blog/script.js
+++ blog/script.js
@@ -43,6 +43,7 @@ const allPosts = [
   {href:"/2019-04-02-attinyapp", tags: ["#itp", "#homehard"]},
   {href:"/2019-04-08-eagleboard", tags: ["#itp", "#homehard"]},
   {href:"/2019-04-11-sunadd", tags: ["#itp", "#energy"]},
+  {href:"/2019-04-19-freestoremac", tags: ["#itp", "#freestore"]},
 ]
 
 const allTags = _(allPosts).chain().flatMap("tags").uniq().value()
