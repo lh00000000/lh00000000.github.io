67206bcf6265cd816802581f5e587d9f763c544d
Luming Hao <lh00000000@gmail.com>
2019-04-22 03:00:51 -0400

    el library

--- template/script.js
+++ template/script.js
@@ -1,3 +1,36 @@
+const el = {
+  img(imgSrc) {
+    let ele = document.createElement("img")
+    ele.src = imgSrc
+    return ele
+  },
+  p(text) {
+    let ele = document.createElement("p")
+    ele.innerText = text
+    return ele
+  },
+  span(text) {
+    let ele = document.createElement("span")
+    ele.innerText = text
+    return ele
+  },
+  div(child) {
+    let ele = document.createElement("div")
+    ele.appendChild(child)
+    return ele
+  }
+}
+
+const ap = (selector, child) => {
+  document.querySelector(selector).appendChild(child)
+  return document.querySelector(selector)
+}
+
+const cl = (ele, classes) => {
+  classes.forEach(ele.classList.add)
+  return ele
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
 })
