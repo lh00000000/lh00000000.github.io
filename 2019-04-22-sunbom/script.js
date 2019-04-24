3c6dce7934b6295a710be210df9a2183e463a33a
Luming Hao <lh00000000@gmail.com>
2019-04-23 23:50:01 -0400

    bom verbrose

--- 2019-04-22-sunbom/script.js
+++ 2019-04-22-sunbom/script.js
@@ -1,23 +1,35 @@
 const el = {
-  src(tag, toSrc) {
-    let ele = document.createElement(tag)
+  src(ele, toSrc) {
+    if (typeof ele == "string") {ele = document.createElement(ele)}
     ele.src = toSrc
     return ele
   },
-  inn(tag, innerML) {
-    let ele = document.createElement(tag)
+  inn(ele, innerML) {
+    if (typeof ele == "string") {ele = document.createElement(ele)}
     ele.innerHTML = innerML
     return ele
   },
-  txt(tag, text) {
-    let ele = document.createElement(tag)
+  txt(ele, text) {
+    if (typeof ele == "string") {ele = document.createElement(ele)}
     ele.innerText = text
     return ele
   },
-  cls(tag, classStr) {
-    let ele = document.createElement(tag)
+  cls(ele, classStr) {
+    if (typeof ele == "string") {ele = document.createElement(ele)}
     classStr.split(" ").forEach(c => ele.classList.add(c.substring(1)))
     return ele
+  },
+  a(url, inside) {
+    let aEle = document.createElement("a")
+    aEle.href = url
+
+    if (typeof inside == "string") {
+      aEle.innerText = inside
+      return aEle
+    } else {
+      return ap(aEle, inside)
+    }
+
   }
 }
 
@@ -30,67 +42,84 @@ const ap = (up, child) => {
 
 let bom = [{
   what: "charge controller",
-  id: "MCP73871",
+  partno: "MCP73871",
   quantity: 1,
-  cart: "https://www.digikey.com/product-detail/en/microchip-technology/MCP73871T-2CCI-ML/MCP73871T-2CCI-MLCT-ND/7065594",
+  source: "https://www.digikey.com/product-detail/en/microchip-technology/MCP73871T-2CCI-ML/MCP73871T-2CCI-MLCT-ND/7065594",
   datasheet: "http://ww1.microchip.com/downloads/en/DeviceDoc/20002090D.pdf",
   cost: 1.84
 }, {
   what: "power mux",
-  id: "TPS2115A",
+  partno: "TPS2115A",
   quantity: 1,
-  cart: "https://www.digikey.com/product-detail/en/texas-instruments/TPS2115APWR/296-16940-1-ND/652794",
+  source: "https://www.digikey.com/product-detail/en/texas-instruments/TPS2115APWR/296-16940-1-ND/652794",
   datasheet: "http://www.ti.com/general/docs/suppproductinfo.tsp?distId=10&gotoUrl=http%3A%2F%2Fwww.ti.com%2Flit%2Fgpn%2Ftps2114a",
   cost: 2.41
 }, {
-  what: "diode",
-  id: undefined,
+  what: "diode (THRU HOLE)",
+  partno: "N/A",
   quantity: 3,
-  cart: undefined,
-  datasheet: undefined,
-  cost: undefined
+  source: "ITP.SHOP",
+  datasheet: "N/A",
+  cost: "NYUTUITION"
 }, {
   what: "10k resistor",
-  id: undefined,
+  partno: "N/A",
   quantity: 5,
-  cart: undefined,
-  datasheet: undefined,
-  cost: 0
+  source: "ITP.HARDLAB",
+  datasheet: "N/A",
+  cost: "NYUTUITION"
 }, {
   what: "470 resistor",
-  id: undefined,
+  partno: "N/A",
   quantity: 5,
-  cart: undefined,
-  datasheet: undefined,
-  cost: 0
+  source: "ITP.HARDLAB",
+  datasheet: "N/A",
+  cost: "NYUTUITION"
 }, {
   what: "4.7 uF capacitor",
-  id: undefined,
+  partno: "N/A",
   quantity: 2,
-  cart: undefined,
-  datasheet: undefined,
-  cost: 0
+  source: "ITP.HARDLAB",
+  datasheet: "N/A",
+  cost: "NYUTUITION"
 }, {
   what: "2 pin header",
-  id: undefined,
+  partno: "N/A",
   quantity: 2,
-  cart: undefined,
-  datasheet: undefined,
-  cost: undefined
+  source: "https://www.amazon.com/Haobase-2-54mm-Straight-Single-Headers/dp/B01DLX6RSQ/ref=sr_1_5?keywords=HEADER+FEMALE&qid=1556076085&s=gateway&sr=8-5",
+  datasheet: "N/A",
+  cost: 5.60
 }, {
   what: "4 pin header",
-  id: undefined,
+  partno: "N/A",
   quantity: 1,
-  cart: undefined,
-  datasheet: undefined,
-  cost: undefined
+  source: "https://www.amazon.com/Haobase-2-54mm-Straight-Single-Headers/dp/B01DLX6RSQ/ref=sr_1_5?keywords=HEADER+FEMALE&qid=1556076085&s=gateway&sr=8-5",
+  datasheet: "N/A",
+  cost: 5.60
 }]
 
 document.addEventListener("DOMContentLoaded", () => {
   ap("#main", el.txt("h1", "SUNADDBOARD".split("").map((c, i) => c + _.range(i+1).map(x => "_").join("")).join("")))
+
+
+  let explain = el.cls("div", ".explain")
+
+  let p1 = el.txt("p", "")
+  ap(p1, el.txt("span", "GOAL = CREATE CUSTOM HARDWARE FOR !(LOGIC ||INFORMATION) ASPECTS OF "))
+  ap(p1, el.a("https://luminghao.com/2019-04-11-sunadd/", "SUN_ADD 1 BIT SOLAR ADDER"))
+  ap(p1, el.txt("span", ". "))
+  ap(explain, p1)
+
+  ap(explain, el.txt("p", "GOAL.INPUT = [0-6V PANEL A, GROUND PANEL A, 0-6V PANEL B, GROUND PANEL B, POSITIVE LEAD LIPO, GROUND LIPO] "))
+  ap(explain, el.txt("p", "GOAL.OUTPUT = [LOGIC OUT FOR INPUT A, LOGIC OUT FOR INPUT B, GROUND, 5V]"))
+
+  ap("#main", ap(el.cls("div", ".explain"), explain))
+
+  ap("#main", el.src("img", "https://lh00000000-public.s3.amazonaws.com/2019/siteimg/2019-04-22-sunbom/sunbomsch.png"))
+
   bom.forEach((b, i) => {
     let rec = el.cls("div", ".record")
-    _.sortBy(_.toPairs(b)).forEach(([key, val]) => {
+    _.reverse(_.sortBy(_.toPairs(b))).forEach(([key, val]) => {
       let keypair = el.cls("p", ".keypair")
       ap(keypair, el.txt("b", `${key}: `))
       ap(keypair, el.inn("span",
@@ -106,12 +135,10 @@ document.addEventListener("DOMContentLoaded", () => {
     ap("#main", bomBanner)
   })
 
-  ap("#main", el.src("img", "https://lh00000000-public.s3.amazonaws.com/2019/siteimg/2019-04-22-sunbom/sunbomsch.png"))
+
 
   setInterval(() => {
-    _.range(64).forEach(i => {
-      let unlucky = _.sample(...[document.querySelectorAll(".bombanner > span")])
-      unlucky.classList.contains("hide") ? unlucky.classList.remove("hide") : unlucky.classList.add("hide")
-    })
-  }, 200)
+    let allBomban = [...document.querySelectorAll(".bombanner > span")]
+    _.shuffle(allBomban).slice(0, allBomban.length / 2).forEach(b => b.classList.contains("hide") ? b.classList.remove("hide") : b.classList.add("hide"))
+  }, 125)
 })
