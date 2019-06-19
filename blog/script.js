89553de78fa428af2f2169ede8c2d9ed03a381a0
Luming Hao <lh00000000@gmail.com>
2019-06-19 12:43:38 -0400

    squeezehit

--- blog/script.js
+++ blog/script.js
@@ -54,6 +54,7 @@ const allPosts = [
   {href:"/2019-04-22-sunbom", tags: ["#itp", "#homehard"]},
   {href:"/2019-05-02-stretchexp", tags: ["#exp", "#js"]},
   {href:"/2019-05-17-dyesens", tags: ["#itp", "#energy"]},
+  {href:"/2019-06-19-squeezehit", tags: ["#liao", "#audio", "#freq"]},
 ]
 
 const allTags = _(allPosts).chain().flatMap("tags").uniq().value()
