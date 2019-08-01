5fd734531ec6c2dc3a093b9221baa5658316850f
Luming Hao <lh00000000@gmail.com>
2019-08-01 04:10:50 -0400

    there's a new blink in town

--- 2019-08-13-annualoffsite/script.js
+++ 2019-08-13-annualoffsite/script.js
@@ -1,35 +1,62 @@
-const el = {
-  src(tag, toSrc) {
-    let ele = document.createElement(tag)
-    ele.src = toSrc
-    return ele
-  },
-  html(tag, innerML) {
-    let ele = document.createElement(tag)
-    ele.innerHTML = innerML
-    return ele
-  },
-  txt(tag, text) {
-    let ele = document.createElement(tag)
-    ele.innerText = text
-    return ele
-  },
-  cls(tag, classStr) {
-    let ele = document.createElement(tag)
-    classStr.split(" ").forEach(c => ele.classList.add(c.substring(1)))
-    return ele
-  }
+
+
+const toggleColor = () => {
+  let e = document.getElementById("CALTEXTLINK")
+  let currentColor = e.style.color || "black"
+  let newColor = currentColor == "black" ? "white" : "black"
+  e.style.color = newColor
+  return newColor
+
 }
 
-const ap = (up, child) => {
-  let parent = (typeof up == "string") ? document.querySelector(up) : up
-  parent.appendChild(child)
-  return parent
+const pitch = ({audioContext, masterGainNode}, freq, length) => {
+  let now = audioContext.currentTime
+  let oscillator = audioContext.createOscillator()
+  let episilon = 0.001
+  oscillator.type = "sine"
+  oscillator.frequency.value = freq
+  oscillator.connect(masterGainNode)
+  masterGainNode.gain.setValueAtTime(0.8, now)
+  masterGainNode.gain.exponentialRampToValueAtTime(episilon, now + length)
+  oscillator.start(now)
+  oscillator.stop(now + length + episilon)
 }
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   let res = await fetch("https://luminghao.com/2019-08-13-annualoffsite/ANNUAL_OFFSITE.ics")
   let resText = await res.text()
   let daturi = `data:text/calendar,${encodeURIComponent(resText)}`
   document.getElementById("CALTEXTLINK").href = daturi
+
+  let audioContext = new (window.AudioContext || window.webkitAudioContext)
+  let masterGainNode = audioContext.createGain()
+  masterGainNode.connect(audioContext.destination)
+
+  let period = 500
+  let toggleTimeout = () => {
+    let newColor = toggleColor()
+    pitch(
+      {audioContext, masterGainNode},
+      newColor == "black" ? 440 : 220,
+      Math.min(1, (period / 1000) - 0.01)
+    )
+    setTimeout(toggleTimeout, period)
+  }
+  toggleTimeout()
+
+  const startdt = new Date()
+  const lfo = 4096
+  const maxPeriod = 1024
+  const minPeriod = 9.090909
+
+  setInterval(() => {
+    let sinceStart = (new Date()) - startdt
+    let deci = (Math.sin(sinceStart / lfo) + 1) / 2
+    period = (deci * maxPeriod) + minPeriod
+  }, 8)
+
 })
+
+
+
