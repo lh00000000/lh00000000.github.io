7f44f10005bed6fba69004ddf3cfb0adeaa6eab5
Luming Hao <lh00000000@gmail.com>
2019-10-08 19:33:58 -0400

    fuck it i'm using react

--- template/script.js
+++ template/script.js
@@ -1,32 +1,49 @@
-const el = {
-  src(tag, toSrc) {
-    let ele = document.createElement(tag)
-    ele.src = toSrc
-    return ele
-  },
-  html(tag, innerML) {
-    let ele = document.createElement(tag)
-    ele.innerHTML = innerML
-    return ele
-  },
-  txt(tag, text) {
-    let ele = document.createElement(tag)
-    ele.innerText = text
-    return ele
-  },
-  cls(tag, classStr) {
-    let ele = document.createElement(tag)
-    classStr.split(" ").forEach(c => ele.classList.add(c.substring(1)))
-    return ele
-  }
+const imgs = {
+    a:
+        "https://www.delphfishing.com/wp-content/gallery/wahoo/wahoo-jan-2015.jpg"
 }
 
-const ap = (up, child) => {
-  let parent = (typeof up == "string") ? document.querySelector(up) : up
-  parent.appendChild(child)
-  return parent
-}
+const imgCap = (imgSrc, caption, width = 600) => (
+    <div style={{ width }}>
+        <div>
+            <img style={{ width: "100%" }} src={imgs[imgSrc]} />
+        </div>
+        <div>
+            <p style={{ margin: 0 }}>{caption}</p>
+        </div>
+    </div>
+)
 
-document.addEventListener("DOMContentLoaded", () => {
+const imgRCap = (imgSrc, caption, width = 600) => (
+    <div style={{ display: "flex", flexDirection: "row", width }}>
+        <div>
+            <img style={{ width: width/2 }} src={imgs[imgSrc]} />
+        </div>
+        <div>
+            <p  style={{ width: width/2 }}  style={{ margin: 0 }}>{caption}</p>
+        </div>
+    </div>
+)
+const list = (title, items) => (
+    <div style={{ width: 480 }}>
+        <div>
+            <p>{title}</p>
+        </div>
+        <div>
+            <ul>
+                {items.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+)
 
-})
+const nbt = txts => txts.join("")
+ReactDOM.render(
+    <div>
+        {imgCap("a", "bottom caption")}
+        {imgRCap("a", "right caption")}
+    </div>,
+    document.getElementById("main")
+)
