03a5fdd21381447925ba0455275a352cee8a302c
Luming Hao <lh00000000@gmail.com>
2019-10-08 21:08:45 -0400

    imgsrc blog template

--- template/script.js
+++ template/script.js
@@ -6,7 +6,7 @@ const imgs = {
 const imgCap = (imgSrc, caption, width = 600) => (
     <div style={{ width }}>
         <div>
-            <img style={{ width: "100%" }} src={imgs[imgSrc]} />
+            <img style={{ width: "100%" }} src={imgSrc} />
         </div>
         <div>
             <p style={{ margin: 0 }}>{caption}</p>
@@ -17,7 +17,7 @@ const imgCap = (imgSrc, caption, width = 600) => (
 const imgRCap = (imgSrc, caption, width = 600) => (
     <div style={{ display: "flex", flexDirection: "row", width }}>
         <div>
-            <img style={{ width: width / 2 }} src={imgs[imgSrc]} />
+            <img style={{ width: width / 2 }} src={imgSrc} />
         </div>
         <div>
             <p style={{ width: width / 2 }} style={{ margin: 0 }}>
