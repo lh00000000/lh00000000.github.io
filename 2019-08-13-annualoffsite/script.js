06c50a0e9487b2aabf7ac95961c87858c207cb0a
Luming Hao <lh00000000@gmail.com>
2019-11-25 18:11:05 -0500

    annualoffset assets

--- 2019-08-13-annualoffsite/script.js
+++ 2019-08-13-annualoffsite/script.js
@@ -1,15 +1,12 @@
-
-
 const toggleColor = () => {
   let e = document.getElementById("CALTEXTLINK")
   let currentColor = e.style.color || "black"
   let newColor = currentColor == "black" ? "white" : "black"
   e.style.color = newColor
   return newColor
-
 }
 
-const pitch = ({audioContext, masterGainNode}, freq, length) => {
+const pitch = ({ audioContext, masterGainNode }, freq, length) => {
   let now = audioContext.currentTime
   let oscillator = audioContext.createOscillator()
   let episilon = 0.001
@@ -21,8 +18,6 @@ const pitch = ({audioContext, masterGainNode}, freq, length) => {
   oscillator.start(now)
   oscillator.stop(now + length + episilon)
 }
-
-
 
 const freqs = {
   CS6: 1108.731,
@@ -42,29 +37,29 @@ let baseFreq = _.sample([
   freqs.A4,
   freqs.D4,
   freqs.D4,
-  freqs.D4,
+  freqs.D4
 ])
 document.addEventListener("DOMContentLoaded", async () => {
-  let res = await fetch("https://luminghao.com/2019-08-13-annualoffsite/ANNUAL_OFFSITE.ics")
+  let res = await fetch(
+    "https://luminghao.com/2019-08-13-annualoffsite/ANNUAL_OFFSITE.ics"
+  )
   let resText = await res.text()
   let daturi = `data:text/calendar,${encodeURIComponent(resText)}`
   document.getElementById("CALTEXTLINK").href = daturi
 
-  let audioContext = new (window.AudioContext || window.webkitAudioContext)
+  let audioContext = new (window.AudioContext || window.webkitAudioContext)()
   let masterGainNode = audioContext.createGain()
   masterGainNode.connect(audioContext.destination)
 
-  let period = 512
-  let toggleTimeout = () => {
+  let period = 512(() => {
     let newColor = toggleColor()
     pitch(
-      {audioContext, masterGainNode},
-      newColor == "black" ? baseFreq : (baseFreq / 2),
-      Math.min(1, (period / 1000) - 0.01)
+      { audioContext, masterGainNode },
+      newColor == "black" ? baseFreq : baseFreq / 2,
+      Math.min(1, period / 1000 - 0.01)
     )
     setTimeout(toggleTimeout, period)
-  }
-  toggleTimeout()
+  })()
 
   const startdt = new Date()
   const lfo = 4096
@@ -72,15 +67,16 @@ document.addEventListener("DOMContentLoaded", async () => {
   const minPeriod = 9.090909
 
   setInterval(() => {
-      let sinceStart = (new Date()) - startdt
-      let deci = (Math.sin(sinceStart / lfo) + 1) / 2
-      period = (deci * maxPeriod) + minPeriod
-      window.requestAnimationFrame(adjustPeriod)
+    let sinceStart = new Date() - startdt
+    let deci = (Math.sin(sinceStart / lfo) + 1) / 2
+    period = deci * maxPeriod + minPeriod
+    window.requestAnimationFrame(adjustPeriod)
   }, 16)
 
   // start sound on ios
-  document.querySelector("body").addEventListener("touchend", () => pitch({audioContext, masterGainNode}, 110, 1))
+  document
+    .querySelector("body")
+    .addEventListener("touchend", () =>
+      pitch({ audioContext, masterGainNode }, 110, 1)
+    )
 })
-
-
-
