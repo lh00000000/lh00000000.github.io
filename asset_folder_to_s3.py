20f7dd09bfe456cde86c12210299dab9bb459148
Luming Hao <lh00000000@gmail.com>
2019-12-03 17:48:34 -0500

    nicer keys for asset dict script

--- asset_folder_to_s3.py
+++ asset_folder_to_s3.py
@@ -8,7 +8,7 @@ from subprocess import call
 s3_client = boto3.client("s3")
 folder_prefix = "2019/siteassets"
 url_prefix = "https://lh00000000-public.s3.us-east-1.amazonaws.com/"
-keys_done = []
+key_lookup = {}
 local_asset_folder_name = sys.argv[1]
 
 
@@ -18,10 +18,9 @@ for filepath in glob.glob(f"./{local_asset_folder_name}/*"):
     nice_filename = os.path.basename(filepath).replace(" ", "-")
     key_str = f"{folder_prefix}/{local_asset_folder_name}/{nice_filename}"
     res = s3_client.upload_file(filepath, "lh00000000-public", key_str)
-    keys_done.append(url_prefix + key_str)
+    key_lookup[nice_filename.split(".")[0]] = url_prefix + key_str
 
 
-key_lookup = dict(enumerate(keys_done))
 # json.dump(key_lookup, open(f"assets-{local_asset_folder_name}.json", "w"))
 
 print(json.dumps(key_lookup))
