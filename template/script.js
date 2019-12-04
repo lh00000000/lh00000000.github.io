630786b8f0d45a766e06b163d0bd2f2580f7a39c
Luming Hao <lh00000000@gmail.com>
2019-12-03 22:41:16 -0500

    components react for template

--- template/script.js
+++ template/script.js
@@ -3,49 +3,83 @@ const assets = {
         "https://www.delphfishing.com/wp-content/gallery/wahoo/wahoo-jan-2015.jpg"
 }
 
-const imgCap = (imgSrc, caption, width = 600) => (
-    <div style={{ width }}>
-        <div>
-            <img style={{ width: "100%" }} src={imgSrc} />
-        </div>
-        <div>
-            <p style={{ margin: 0 }}>{caption}</p>
-        </div>
-    </div>
-)
+const DEFAULT_COL_WIDTH = 600
 
-const imgRCap = (imgSrc, caption, width = 600) => (
-    <div style={{ display: "flex", flexDirection: "row", width }}>
+
+class CaptionedImage extends React.Component {
+    constructor(props) {
+        props.width = props.width || DEFAULT_COL_WIDTH
+        super(props)
+    }
+    rightStyle() {
+        return <div style={{ display: "flex", flexDirection: "row", width: this.props.width }}>
         <div>
-            <img style={{ width: width / 2 }} src={imgSrc} />
+            <img style={{ width: this.props.width / 2 }} src={src} />
         </div>
         <div>
-            <p style={{ width: width / 2 }} style={{ margin: 0 }}>
-                {caption}
+            <p style={{ width: this.props.width / 2 }} style={{ margin: 0 }}>
+                {this.props.caption}
             </p>
         </div>
     </div>
-)
-const list = (title, items) => (
-    <div style={{ width: 480 }}>
+    }
+
+    verticalStyle() {
+        return
+            <div style={{ width: this.props.width }}>
+                <div>
+                    <img style={{ width: "100%" }} src={this.props.src} />
+                </div>
+                <div>
+                    <p style={{ margin: 0 }}>{this.props.caption}</p>
+                </div>
+            </div>
+    }
+
+    render() {
+        return this.props.right ? this.rightStyle() : this.verticalStyle()
+    }
+}
+
+
+class List extends React.Component {
+    constructor(props) {
+        props.width = props.width || DEFAULT_COL_WIDTH
+        props.ordered = props.ordered || false
+        super(props)
+    }
+
+    render() {
+        let innerItems = this.props.items.map(item => (
+                    <li>{item}</li>
+                ))
+        let list = this.props.width ? <ol>{innerItems}</ol> : <ul>{innerItems}</ul>
+        return <div style={{ width: 480 }}>
         <div>
-            <p>{title}</p>
+            <p>{this.props.text}</p>
         </div>
         <div>
-            <ul>
-                {items.map(item => (
-                    <li>{item}</li>
-                ))}
-            </ul>
+            {list}
         </div>
     </div>
-)
 
-let mov = (src, width = 320, height = 240) => (
-    <video width={width} height={height} controls>
-        <source src={src} type={`video/${_.last(src.split("."))}`} />
-    </video>
-)
+    }
+}
+
+
+
+class Movie extends React.Component {
+    constructor(props) {
+        props.width = props.width || 320
+        props.width = props.width || 240
+        super(props)
+    }
+    render() {
+        return <video width={this.props.width} height={this.props.height} controls>
+            <source src={this.props.src} type={`video/${_.last(src.split("."))}`} />
+        </video>
+    }
+}
 
 class PageCentered extends React.Component {
     render() {
@@ -73,6 +107,16 @@ class TotalCenter extends React.Component {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            height: "100%"
+        }
+        return <div style={style}>{this.props.children}</div>
+    }
+}
+
+class Fullscreen extends React.Component {
+    render() {
+        let style = {
+            width: "100wh",
             height: "100vh"
         }
         return <div style={style}>{this.props.children}</div>
@@ -84,7 +128,7 @@ $(document).ready(() => {
     hljs.initHighlightingOnLoad()
 
     ReactDOM.render(
-        <TotalCenter>{imgCap(assets["a"], "bottom caption")}</TotalCenter>,
+        <PageCentered>{imgCap(assets["a"], "bottom caption")}</PageCentered>,
         document.getElementById("main")
     )
 })
