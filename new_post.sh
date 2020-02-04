cd1944579c59cbee745ce5cae2e49b7541f62d7d
Luming Hao <lh00000000@gmail.com>
2020-02-04 00:51:41 -0500

    regeant tut

--- new_post.sh
+++ new_post.sh
@@ -4,3 +4,4 @@ FULL_ID="$(date +%Y-%m-%d)-$NO_DATE_ID"
 # echo $FULL_ID
 cp -R template/ "$FULL_ID"
 sed -i '' "s/{{blogId}}/$FULL_ID/g" "$FULL_ID/index.html"
+sed -i '' "s/{{blogId}}/$FULL_ID/g" "$FULL_ID/src-cljs/blogpost/core.cljs"
