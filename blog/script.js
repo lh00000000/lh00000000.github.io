d042c11411c284b33171e2fa58b3fc3525c43084
Luming Hao <lh00000000@gmail.com>
2020-02-11 12:25:23 -0500

    biodes01

--- blog/script.js
+++ blog/script.js
@@ -3,7 +3,7 @@ const FILTERED_SOME = "FILTERED_SOME"
 const FILTERED_ALL = "FILTERED_ALL"
 
 const allPosts = [
-  {href:"/2020-02-04-biodes00", tags: ["#biodes", "#itp"]},
+  {href:"/2020-02-11-biodes01", tags: ["#biodes", "#itp"]},
   {href:"/2020-02-10-itpthesis01", tags: ["#itpthesis", "#itp"]},
   {href:"/2020-02-10-tuneclass02", tags: ["#tuneclass"]},
   {href:"/2020-02-10-karaliao", tags: ["#liao"]},
