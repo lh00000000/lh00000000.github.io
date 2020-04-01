7b06bb9f146f562fb7ec341ece6e4f752f213a78
Luming Hao <lh00000000@gmail.com>
2020-04-01 01:33:06 -0400

    mv old template

---
+++ template-lein/src-cljs/blogpost/core.cljs
@@ -0,0 +1,80 @@
+(ns blogpost.core
+  (:require [clojure.string :as string]
+            [reagent.core :as r]))
+
+(enable-console-print!)
+
+(def app-state (r/atom {:count 0}))
+(defn up! [] (swap! app-state update-in [:count] inc))
+
+(defn captioned-img [imgsrc caption]
+  [:figure
+   [:a {:href imgsrc}
+    [:img {:alt caption :src imgsrc :width "100%"}]]
+   [:figcaption [:span [:i caption]]]])
+
+(defn flexcols [cols & items]
+  [:div {:style {:max-width "90vw"
+                 :display "flex"
+                 :flex-wrap "wrap"}}
+   (map (fn [d] [:div {:style {:flex (str (/ 98 cols) "% 0 1")}} d]) items)])
+
+(defn extension [s]
+  (string/lower-case (last (string/split s #"\."))))
+
+(defn audio [src]
+  [:audio {:controls "controls" :style {:width "100%"}}
+   [:source {:src src :type (str "audio/" (extension src))}]])
+
+(defn video [src]
+  (let [video-type (str "video/ " ((extension src)
+                                   (hash-map "mov" "mp4")))]
+    [:video {:width "100%" :controls "controls"}
+     [:source {:src src :type video-type}]]))
+
+(defn toc [post-title summary & sections]
+  [:div#layout
+   [:article#mainarticle
+    [:h1 post-title]
+
+    [:section
+     [:div summary]
+     [:div
+      (interpose [:span " / "] (map
+                                (fn [{:keys [title]}] [:span [:a {:href (str "#" title)}
+                                                              title]])
+                                sections))]]
+    (map :section sections)
+    [:div {:style {:height "300px"}}]
+    ]])
+
+(defn tsection [titlecode & contents]
+  {:title titlecode
+   :section
+   [:section {:id titlecode}
+    [:h3 titlecode]
+    contents]})
+
+(def lorem
+  (tsection "lorem"
+    [:p "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."]
+    (captioned-img "https://www.northerntool.com/images/product/2000x2000/415/41593_2000x2000.jpg" "test image")
+ )
+)
+
+(def interaction
+  (tsection "interaction"
+    [:p (str "count: " (:count @app-state))]
+    [:button {:on-click up!} "up"]
+  )
+)
+
+(defn start []
+  (r/render-component
+   (toc "{{blogId}}"
+        [:p "some summary"]
+        wavdemo
+        essay
+        interaction
+        )
+   (.getElementById js/document "root")))
