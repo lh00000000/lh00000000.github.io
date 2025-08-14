ca97d784515fa98bbde8006ee3d9d9fb039f8377
GitHub Action <action@github.com>
2025-08-14 23:37:52 +0000

    Update blog artifacts [skip ci]

--- bday34/sw.js
+++ bday34/sw.js
@@ -5,7 +5,7 @@ const RUNTIME_CACHE = 'bday34-runtime-' + CACHE_VERSION;
 const PRECACHE_ASSETS = [
   './',
   './index.html',
-  './assets/index-BhK_jFlZ.js',
+  './assets/index-D28zdq7W.js',
   './assets/index-B0mmezy0.css',
   './vite.svg'
 ];
